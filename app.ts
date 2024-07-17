16ec63606698e773c83f03df749ddb2215113991
Abubakr <abubakrdev69@gmail.com>
2024-07-17 18:27:29 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,61 @@
+// function makeNegative(num: number): number {
+//   return num > 0 ? -num : num;
+// }
+
+// //2
+// function evenOrOdd(num: number): string {
+//   return num % 2 === 0 ? 'Even' : 'Odd';
+// }
+
+
+// //3
+// function opposite(num: number): number {
+//   return -num;
+// }
+
+
+// //4
+// function numberToString(num: number): string {
+//   return num.toString();
+// }
+
+
+// //5
+// function removeChar(str: string): string {
+//   return str.slice(1, -1);
+// }
+
+
+// //6
+// function centuryFromYear(year: number): number {
+//   return Math.ceil(year / 100);
+// }
+
+
+// //7
+// function basicOp(operation: string, value1: number, value2: number): number {
+//   switch (operation) {
+//     case '+': return value1 + value2;
+//     case '-': return value1 - value2;
+//     case '*': return value1 * value2;
+//     case '/': return value1 / value2;
+//     default: throw new Error('Unknown operation');
+//   }
+// }
+
+
+// //8
+// function positiveSum(arr: number[]): number {
+//   return arr.reduce((sum, num) => num > 0 ? sum + num : sum, 0);
+// }
+
+// //9
+// function repeatStr(n: number, s: string): string {
+//   return s.repeat(n);
+// }
+
+
+// //10
+// function noSpace(x: string): string {
+//   return x.replace(/\s/g, '');
+// }
